b0ed5aed731c4bf5a6aa8d610ee852463894b6d7
Mateo Araque <mateo.araque@udla.edu.ec>
2023-05-23 13:18:04 -0500

    entrega final

--- matriz.c
+++ matriz.c
@@ -42,4 +42,39 @@ int main() {
     printf("Ingrese el número de columnas de la matriz: ");
     scanf("%d", &columnas);
 
-   
+    // Reservar memoria para la matriz
+    int **matriz = (int **)malloc(filas * sizeof(int *));
+    for (int i = 0; i < filas; i++) {
+        matriz[i] = (int *)malloc(columnas * sizeof(int));
+    }
+
+    // Generar números aleatorios y llenar la matriz
+    srand(time(0)); // Inicializar la semilla aleatoria
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = generarNumeroAleatorio();
+        }
+    }
+
+    printf("Matriz original:\n");
+    imprimirMatriz(matriz, filas, columnas);
+
+    // Calcular la matriz transpuesta
+    int **matrizTranspuesta = calcularMatrizTranspuesta(matriz, filas, columnas);
+
+    printf("Matriz transpuesta:\n");
+    imprimirMatriz(matrizTranspuesta, columnas, filas);
+
+    // Liberar la memoria de la matriz y la matriz transpuesta
+    for (int i = 0; i < filas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+
+    for (int i = 0; i < columnas; i++) {
+        free(matrizTranspuesta[i]);
+    }
+    free(matrizTranspuesta);
+
+    return 0;
+}
